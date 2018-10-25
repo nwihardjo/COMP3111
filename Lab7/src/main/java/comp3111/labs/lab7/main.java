70fa2a1fd07b844233fb782fb66838b1f408626d
Nathaniel Wihardjo <nwihardjo@connect.ust.hk>
2018-10-26 01:02:49 +0800

    added Lab7

---
+++ Lab7/src/main/java/comp3111/labs/lab7/main.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package comp3111.labs.lab7;
+
+/**
+ * @author kevinw
+ * 
+ * The coding style is bad in purpose.
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Animal animals[] = new Animal[10];
+		for (int i = 0; i < animals.length; i++)
+			animals[i] = new Animal();
+		
+		for (int iii = 0; iii < 10; iii++) {
+			int ii = 0;
+			for (; ii < 100 && animals[iii].isAlive() ; ii++) {
+				System.out.print(animals[iii].getWeight() + " ");
+				animals[iii].eat();
+				if (ii % 3 == 0)
+				animals[iii].poo();
+			}
+		}
+
+	}
+	
+
+}
---
+++ Lab7/src/test/java/comp3111/labs/lab7/AnimalTest.java
@@ -0,0 +1,45 @@
+package comp3111.labs.lab7;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class AnimalTest {
+
+	@Test
+	public void testInitWeight() {
+		Animal a = new Animal();
+		assertEquals((int)a.getWeight(), 5);
+	}
+	
+	@Test
+	public void testInitAlive() {
+		Animal a = new Animal();
+		assertEquals((boolean)a.isAlive(), true);
+	}
+	
+	@Test
+	public void testDieByFeedingTooMuch() {
+		Animal a = new Animal();
+		for (int i = 0; i < 100; i++)
+			a.eat();
+		assertEquals(a.isAlive(), false);
+	}
+
+	@Test
+	public void testDieByPoopingTooMuch() {
+		Animal a = new Animal();
+		for (int i = 0; i < 6; i++)
+			a.poo();
+		assertEquals(a.isAlive(), false);
+	}
+	
+	@Test
+	public void testPoopAlreadyDead() {
+		Animal a = new Animal();
+		for (int i = 0; i < 6; i++)
+			a.poo();
+		ver
+	}
+}
